4e39b6f1f4ceea8ac3b97088fd17f7d26c83ec24
cloudycotton <168998965+cloudycotton@users.noreply.github.com>
2025-12-17 19:43:28 +0530

    feat: add support for runable website runtime

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwind from "@tailwindcss/vite"
-import visualPlugin from "@runablehq/vite-editor-plugin"
 import path from "path";
+import runableWebsiteRuntime from "runable-website-runtime"
 
 export default defineConfig({
-	plugins: [react(), visualPlugin(), cloudflare(), tailwind()],
+	plugins: [react(), runableWebsiteRuntime(), cloudflare(), tailwind()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src/web"),
